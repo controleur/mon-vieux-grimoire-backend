8372dcfe5c163343a58da7dedb23e22fc5a90096
Controleur <18554067+controleur@users.noreply.github.com>
2025-05-06 19:00:30 +0300

    ajout connexion bdd

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Book = require('./models/book');
 
+mongoose.connect('mongodb+srv://devcontroleur:p4KIFGLIWBxYfALZ@cluster0.hujly5r.mongodb.net/test?retryWrites=true&w=majority')
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
+
 
 
 module.exports = app;
