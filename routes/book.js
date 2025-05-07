6cda5566b1671c83e52681401ab66b3ba99dbaca
Controleur <18554067+controleur@users.noreply.github.com>
2025-05-07 17:18:12 +0300

    ajout multer aux routes book

--- routes/book.js
+++ routes/book.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bookCtrl = require('../controllers/book');
 const auth = require('../middleware/auth');
+const multer = require('multer');
 
-router.post('/', auth, bookCtrl.createBook);
-router.put('/:id', auth, bookCtrl.updateBook);
+router.post('/', auth, multer, bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.showOneBook);
 router.get('/' + '', bookCtrl.showAllBooks);
