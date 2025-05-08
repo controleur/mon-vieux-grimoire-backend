3b05f3087835b3fe67d65eb36f82f8ab9a1dbd8b
Controleur <18554067+controleur@users.noreply.github.com>
2025-05-08 13:23:08 +0300

    modification updateBook pour permettre changement d'image

--- controllers/book.js
+++ controllers/book.js
@@ -54,24 +54,22 @@ exports.deleteBook = (req, res, next) => {
 };
 
 exports.updateBook = (req, res, next) => {
-  const book = new Book({
-    _id: req.params.id,
-    userId: req.body.userId,
-    title: req.body.title,
-    author: req.body.author,
-    imageUrl: req.body.imageUrl,
-    year: req.body.year,
-    genre: req.body.genre,
-  });
-  Book.updateOne({ _id: req.params.id }, book)
-    .then(() => {
-      res.status(201).json({
-        message: "Livre modifié avec succès!",
+  const bookObject = req.file ? {
+      ...JSON.parse(req.body.book),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+  delete bookObject._userId;
+  Book.findOne({_id: req.params.id})
+      .then((book) => {
+          if (book.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Non autorisé'});
+          } else {
+              Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Livre modifié!'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
       });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error,
-      });
-    });
 };
