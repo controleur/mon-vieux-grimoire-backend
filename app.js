7a5ee8738f707723ca1a477f4c76e12c41fe6c8f
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-22 19:14:55 +0300

    change cors policies

--- app.js
+++ app.js
@@ -19,7 +19,16 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
+
+// Route OPTIONS globale pour CORS
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.sendStatus(200);
+});
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Route pour la documentation Swagger
