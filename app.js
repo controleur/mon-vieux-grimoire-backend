64f2d09df67b30d8594b5e78c4cafbf0e5340577
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-29 17:43:14 +0300

    CORS policies type fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 // Route OPTIONS globale pour CORS
-app.options('*', (req, res) => {
+app.options('/{*any}', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
